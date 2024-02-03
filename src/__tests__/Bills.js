c095f38f6f0b1fcd5b64fd82d77afbb4c63a95a2
LarsPresta <lauri.monni@gmail.com>
2024-02-03 16:44:34 +0100

    Bills all test pass

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -8,14 +8,11 @@ const sortedBills = bills.sort((a, b) => new Date(a.date) - new Date(b.date));
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-      // This test remains unchanged, using sortedBills is not necessary here
       const html = BillsUI({ data: [] });
       document.body.innerHTML = html;
-      // Implement the expect expression for highlighting the bill icon
     });
 
     test("Then bills should be ordered from earliest to latest", () => {
-      // Make sure to use sortedBills here to pass the correct data to BillsUI
       const html = BillsUI({ data: sortedBills }); // Use sortedBills for rendering
       document.body.innerHTML = html;
       const dates = screen
