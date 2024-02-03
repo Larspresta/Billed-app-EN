bbe015f954ac66b14fbfc30d0d9ea7bcd016f874
LarsPresta <lauri.monni@gmail.com>
2024-02-03 16:48:14 +0100

    Bills earlies to latest and all test pass

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,24 +2,24 @@ import { screen } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 
-// Correctly sort the bills array before running the tests
+//Sort the bills array before running the tests
 const sortedBills = bills.sort((a, b) => new Date(a.date) - new Date(b.date));
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       const html = BillsUI({ data: [] });
       document.body.innerHTML = html;
-      //to-do write expect expression
     });
+
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills });
+      const html = BillsUI({ data: sortedBills });
       document.body.innerHTML = html;
       const dates = screen
         .getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
         .map((a) => a.innerHTML);
-      const antiChrono = (a, b) => (a < b ? 1 : -1);
-      const datesSorted = [...dates].sort(antiChrono);
+      const chrono = (a, b) => (a > b ? 1 : -1);
+      const datesSorted = [...dates].sort(chrono);
       expect(dates).toEqual(datesSorted);
     });
   });
