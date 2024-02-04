8d1904699eda1780069c06adbc61a45cdc5f522c
LarsPresta <lauri.monni@gmail.com>
2024-02-04 17:12:46 +0100

    employee test path pdf

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -53,6 +53,7 @@ export default class NewBill {
   };
 
   // no need to cover this function by tests
+  /* istanbul ignore next */
   createBill = (bill) => {
     if (this.firestore) {
       this.firestore
